92f2eb575a6e923ab108f01f97bb06a99b19bd21
blancocd <blancocedi@gmail.com>
2025-06-03 13:23:27 +0200

    original and modified images

--- results/front/remove/cut_image.py
+++ results/front/remove/cut_image.py
@@ -7,18 +7,22 @@ input_dirs = ['/home/blancocd/Development/SU25/Avatar25/inpaint-fluxfill/results
 for input_dir in input_dirs:
     for input_path in os.listdir(input_dir):
         image_fn = os.path.join(input_dir, input_path)
+        if os.path.isdir(image_fn):
+            continue
+        os.makedirs(os.path.join(input_dir, 'original'), exist_ok=True)
+        os.makedirs(os.path.join(input_dir, 'results'), exist_ok=True)
 
         img = Image.open(image_fn)
         width, height = img.size
-
-        # Calculate new width (one third of original)
         new_width = width // 3
 
-        # Define the crop box for the last third (left, upper, right, lower)
-        crop_box = (width - new_width, 0, width, height)
+        original_crop_box = (0, 0, new_width, height)
+        cropped_img = img.crop(original_crop_box)
+        number_part = os.path.splitext(os.path.basename(image_fn))[0].split('_')[0]
+        cropped_img.save(os.path.join(input_dir, 'original', number_part + ".png"))
 
-        # Crop the image
-        cropped_img = img.crop(crop_box)
+        result_crop_box = (width - new_width, 0, width, height)
+        cropped_img = img.crop(result_crop_box)
+        number_part = os.path.splitext(os.path.basename(image_fn))[0].split('_')[0]
+        cropped_img.save(os.path.join(input_dir, 'results', number_part + ".png"))
 
-        # Save the cropped image
-        cropped_img.save(image_fn)
