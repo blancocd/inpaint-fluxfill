524d09625e7bff94ac4b4785d6930ad1ce3d3d55
Cesar Diaz Blanco <pba534@galvani-cn003.sdn>
2025-06-03 17:13:10 +0200

    pipeline for nonmasked garment ready

---
+++ metrics/gemma/infer.py
@@ -0,0 +1,97 @@
+from transformers import pipeline, AutoProcessor, Gemma3ForConditionalGeneration
+import torch
+import json
+import numpy as np
+import os
+from tqdm import tqdm
+from PIL import Image
+
+model_id = "google/gemma-3-4b-it"
+model = Gemma3ForConditionalGeneration.from_pretrained(
+    model_id, device_map="auto"
+).eval()
+processor = AutoProcessor.from_pretrained(model_id)
+
+# pipe = pipeline(
+#     "image-text-to-text",
+#     model="google/gemma-3-4b-it",
+#     device="cuda",
+#     torch_dtype=torch.bfloat16
+# )
+
+input_dir = '/mnt/lustre/work/ponsmoll/pba534/inpaint/results/front/remove/'
+mask_types = ['garment_mask', 'upper_mask']
+
+
+json_path = os.path.join('/mnt/lustre/work/ponsmoll/pba534/inpaint/data/captions/remove_front.json')
+with open(json_path, "r") as f:
+    garments = json.load(f)
+
+def get_key(garments, scan_id, key):
+    for item in garments:
+        if item['scan_id'] == scan_id:
+            return item[key]
+
+for mask_type in mask_types:
+    orig_dir = os.path.join(input_dir, mask_type, 'original')
+    resu_dir = os.path.join(input_dir, mask_type, 'results')
+    for image_fn in tqdm(os.listdir(orig_dir)):
+        scan_id = os.path.splitext(os.path.basename(image_fn))[0].split('.')[0]
+
+        # orig_image = Image.open(os.path.join(orig_dir, image_fn)).convert('RGB')
+        # resu_image = Image.open(os.path.join(resu_dir, image_fn)).convert('RGB')
+
+        inner_garment = get_key(garments, scan_id, 'inner')
+        outer_garment = get_key(garments, scan_id, 'outer')
+        gen_outer_garment = get_key(garments, scan_id, 'general_outer')
+
+        prompt_two_images = f'In the first image there is a person with {inner_garment} as well as a {outer_garment} on top. In the second image the outer garment should have been removed and there should be no outer garment at all. Answer with a simple yes if the person is still wearing a {gen_outer_garment} in the second image. Answer with a simple no if the person is only wearing {inner_garment} in the second image.'
+        prompt_one_image = 'Is the person in the image wearing a jacket, cardigan, sweater, or coat? Answer yes if that is the case and no if the person is only wearing a shirt, t-shirt or blouse.'
+        messages = [[
+            {
+                "role": "system",
+                "content": [{"type": "text", "text": "You are a helpful assistant focused on clothing description."}]
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "image", "path": os.path.join(orig_dir, image_fn)},
+                    {"type": "image", "path": os.path.join(resu_dir, image_fn)},
+                    {"type": "text", "text": prompt_two_images}
+                ]
+            }
+        ],
+        [
+            {
+                "role": "system",
+                "content": [{"type": "text", "text": "You are a helpful assistant focused on clothing description."}]
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "image", "path": os.path.join(resu_dir, image_fn)},
+                    {"type": "text", "text": prompt_one_image}
+                ]
+            }
+        ]
+        ]
+
+        # outputs = pipe(text=messages, images=[orig_image, resu_image], max_new_tokens=200)
+        # print(outputs[0]["generated_text"])
+
+        outputs = []
+        for message in messages:
+            inputs = processor.apply_chat_template(
+                message, add_generation_prompt=True, tokenize=True,
+                return_dict=True, return_tensors="pt"
+            ).to(model.device, dtype=torch.bfloat16)
+            input_len = inputs["input_ids"].shape[-1]
+            with torch.inference_mode():
+                generation = model.generate(**inputs, max_new_tokens=500, do_sample=False)
+                generation = generation[0][input_len:]
+
+            decoded = processor.decode(generation, skip_special_tokens=True)
+            outputs.append(decoded)
+        print(scan_id, mask_type, f'long prompt: {outputs[0]}', f'short prompt: {outputs[1]}')
+
+        
--- remove_garment_two_masks.py
+++ remove_garment_two_masks.py
@@ -5,9 +5,25 @@ import json
 import os.path as osp
 from huggingface_hub import login
 
+from tqdm import tqdm
 import numpy as np
 import os
 from utils.add_bottom_text import add_text_below_image_wrapped
+from PIL import Image
+
+def nand_image(img1, img2):
+    img1_np = np.array(img1.convert('L'))
+    img2_np = np.array(img2.convert('L'))
+
+    # Ensure masks are binary (0 or 255)
+    img1_bin = (img1_np > 127).astype(np.uint8)
+    img2_bin = (img2_np > 127).astype(np.uint8)
+
+    # Compute NAND: invert (img1 AND img2)
+    nand_mask = np.logical_not(np.logical_and(img1_bin, img2_bin)).astype(np.uint8) * 255
+
+    # Convert back to PIL Image
+    return Image.fromarray(nand_mask, mode='L')
 
 token = os.getenv("HUGGINGFACE_TOKEN")
 login(token=token)
@@ -16,66 +32,63 @@ pipe = FluxFillPipeline.from_pretrained("black-forest-labs/FLUX.1-Fill-dev", tor
 # load base and mask image
 base_dir = '/mnt/lustre/work/ponsmoll/pba534/inpaint/'
 data_dir = os.path.join(base_dir, 'data')
-view = 'fb'
-task = 'remove'
-mask_type = 'both_masks'
-results_dir = osp.join(base_dir, 'results', view, task, mask_type)
-os.makedirs(results_dir, exist_ok=True)
+view = 'front'
 
 # Load scans from captions.json
-json_fn = 'captions_remove_fb.json'
-with open(osp.join(base_dir, json_fn), 'r') as f:
+json_fn = 'scan_testset.json'
+with open(osp.join(data_dir, 'captions', json_fn), 'r') as f:
     scans = json.load(f)
 
-specific_scans = ['1828', '1868']
-for mask_interpolation_param in [0.5]: #np.linspace(0.0, 1.0, 5):
-    results_dir = osp.join(base_dir, 'results', view, task, mask_type, 'miprm'+str(mask_interpolation_param))
-    os.makedirs(results_dir, exist_ok=True)
-    for scan in scans:
-        print(f"Testing scan {scan['scan_id']}")
-        scan_id = str(scan['scan_id'])
-        if specific_scans is not None and scan_id in specific_scans:
-           continue 
-        prompts = scan["prompts"]
-        image = load_image(osp.join(base_dir, 'images', view, scan_id + '.png'))
+specific_scans = None # ['1828', '1868']
+results_dir = osp.join(base_dir, 'results', 'nonmasked_garment', view)
 
-        prompt = prompts[0]
-        general_mask_upper = load_image(osp.join(base_dir, 'masks', view, scan_id + '_upper.png'))
-        specific_mask_upper = load_image(osp.join(base_dir, 'masks', view, scan_id + '_upper_garment.png'))
-        
-        gen_image = pipe(
-            prompt=prompt,
-            image=image,
-            mask_image_general=general_mask_upper,
-            mask_image_specific=specific_mask_upper,
-            height=image.height,
-            width=image.width,
-            guidance_scale=30,
-            mask_interpolation_param=mask_interpolation_param,
-            num_inference_steps=50,
-            max_sequence_length=512,
-            generator=torch.Generator("cpu").manual_seed(0)
-        ).images[0]
-        final_img = make_image_grid([image, general_mask_upper, specific_mask_upper, gen_image], 1, 4)
-        final_img = add_text_below_image_wrapped(final_img, prompt)
-        final_img_fn = osp.join(results_dir, f"{scan_id}_undergarment.png")
-        final_img.save(final_img_fn)
-        
-        prompt = ''
-        gen_image = pipe(
-            prompt=prompt,
-            image=image,
-            mask_image_general=general_mask_upper,
-            mask_image_specific=specific_mask_upper,
-            height=image.height,
-            width=image.width,
-            guidance_scale=30,
-            mask_interpolation_param=mask_interpolation_param,
-            num_inference_steps=50,
-            max_sequence_length=512,
-            generator=torch.Generator("cpu").manual_seed(0)
-        ).images[0]
-        final_img = make_image_grid([image, general_mask_upper, specific_mask_upper, gen_image], 1, 4)
-        final_img = add_text_below_image_wrapped(final_img, prompt)
-        final_img_fn = osp.join(results_dir, f"{scan_id}_noprompt.png")
-        final_img.save(final_img_fn)
+os.makedirs(results_dir, exist_ok=True)
+os.makedirs(os.path.join(results_dir, 'grid'), exist_ok=True)
+
+for scan_id, scan in tqdm(enumerate(scans)):
+    scan_id = str(scan_id)
+    if specific_scans is not None and scan_id in specific_scans:
+        continue 
+    
+    image = load_image(osp.join(data_dir, 'images', view, scan_id + '.png'))
+
+    upper_mask = load_image(osp.join(data_dir, 'masks', 'upper', view, scan_id + '.png'))
+    inner_mask = load_image(osp.join(data_dir, 'masks', 'inner', view, scan_id + '.png'))
+    outer_mask = nand_image(upper_mask, inner_mask)
+    
+    gen_image = pipe(
+        prompt=scan['inner'],
+        image=image,
+        mask_image_general=upper_mask,
+        mask_image_specific=outer_mask,
+        height=image.height,
+        width=image.width,
+        guidance_scale=30,
+        mask_interpolation_param=0.,
+        num_inference_steps=50,
+        max_sequence_length=512,
+        generator=torch.Generator("cpu").manual_seed(0)
+    ).images[0]
+
+    gen_image.save(osp.join(results_dir, f"{scan_id}_prompt.png"))
+    img_grid = make_image_grid([image, upper_mask, outer_mask, gen_image], 1, 4)
+    img_grid = add_text_below_image_wrapped(img_grid, scan['inner'])
+    img_grid.save(osp.join(results_dir, 'grid', f"{scan_id}_prompt.png"))
+    
+    gen_image = pipe(
+        prompt='',
+        image=image,
+        mask_image_general=upper_mask,
+        mask_image_specific=outer_mask,
+        height=image.height,
+        width=image.width,
+        guidance_scale=30,
+        mask_interpolation_param=0.,
+        num_inference_steps=50,
+        max_sequence_length=512,
+        generator=torch.Generator("cpu").manual_seed(0)
+    ).images[0]
+    gen_image.save(osp.join(results_dir, f"{scan_id}_noprompt.png"))
+    img_grid = make_image_grid([image, upper_mask, outer_mask, gen_image], 1, 4)
+    final_img_fn = osp.join(results_dir, 'grid', f"{scan_id}_noprompt.png")
+    img_grid.save(final_img_fn)
