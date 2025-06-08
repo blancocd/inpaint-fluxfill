209fe15151db828b21111a057a76acb8e1bc6eda
Cesar Diaz Blanco <pba534@galvani-cn001.sdn>
2025-06-08 13:09:48 +0200

    explicit indication

--- metrics/gemma/evaluate.py
+++ metrics/gemma/evaluate.py
@@ -1,9 +1,10 @@
-from transformers import AutoProcessor, Gemma3ForConditionalGeneration
+from transformers import pipeline, AutoProcessor, Gemma3ForConditionalGeneration
 import torch
 import json
+import numpy as np
 import os
 from tqdm import tqdm
-from diffusers.utils import load_image, make_image_grid
+from PIL import Image
 
 model_id = "google/gemma-3-4b-it"
 model = Gemma3ForConditionalGeneration.from_pretrained(
@@ -14,24 +15,17 @@ processor = AutoProcessor.from_pretrained(model_id)
 images_dir = '/mnt/lustre/work/ponsmoll/pba534/inpaint/data/images/front'
 results_dir = '/mnt/lustre/work/ponsmoll/pba534/inpaint/results/nonmasked_garment/front'
 
+with open('/mnt/lustre/work/ponsmoll/pba534/inpaint/data/captions/scan_testset.json', 'r') as f:
+    garments = json.load(f)
+
 results_list = []
-count_noprompt = 0
-count_prompt = 0
-for idx in tqdm(range(100)):
+count = 0
+for idx, garment_dict in tqdm(enumerate(garments)):
     scan_id = str(idx)
-
-    img = load_image(os.path.join(images_dir, scan_id + '.png'))
-    img_prompt = load_image(os.path.join(results_dir, f'{scan_id}_prompt.png'))
-    img_noprompt = load_image(os.path.join(results_dir, f'{scan_id}_noprompt.png'))
-    
-    os.makedirs(os.path.join(results_dir, 'grid2'), exist_ok=True)
-    make_image_grid([img, img_prompt], 1, 2).save(os.path.join(results_dir, 'grid2', f'{scan_id}_prompt.png'))
-    make_image_grid([img, img_noprompt], 1, 2).save(os.path.join(results_dir, 'grid2', f'{scan_id}_noprompt.png'))
-
-    # prompt = 'Is the person in the image wearing a jacket, cardigan, sweater, or coat? Answer yes if that is the case and no if the person is only wearing a shirt, t-shirt or blouse.'
-    prompt = 'In the image, the person on the left is wearing a jacket, cardigan, sweater, or coat. In the right, the same person **should** not be wearing a top garment and instead only be wearing a shirt, t-shirt or blouse. I am testing a model which removes top garments, is the person on the right wearing the top garment from the left image? Answer yes if that is the case and no if the person is wearing the inner garment.'
+    inner_garm = garment_dict['inner']
+    outer_garm = garment_dict['outer']
+    prompt = f'Is the person in the image wearing a jacket, cardigan, sweater, coat or a {outer_garm}? Answer yes if that is the case and no if the person is only wearing a shirt, t-shirt, blouse or a {inner_garm}.'
     messages = [
-    [
         {
             "role": "system",
             "content": [{"type": "text", "text": "You are a helpful assistant focused on clothing description."}]
@@ -39,54 +33,31 @@ for idx in tqdm(range(100)):
         {
             "role": "user",
             "content": [
-                {"type": "image", "path": os.path.join(results_dir, 'grid2', f'{scan_id}_prompt.png')},
-                {"type": "text", "text": prompt}
-            ]
-        }
-    ],
-    [
-        {
-            "role": "system",
-            "content": [{"type": "text", "text": "You are a helpful assistant focused on clothing description."}]
-        },
-        {
-            "role": "user",
-            "content": [
-                {"type": "image", "path": os.path.join(results_dir, 'grid2', f'{scan_id}_noprompt.png')},
+                {"type": "image", "path": os.path.join(results_dir, f'{scan_id}_prompt.png')},
                 {"type": "text", "text": prompt}
             ]
         }
     ]
-    ]
 
-    outputs = []
-    for message in messages:
-        inputs = processor.apply_chat_template(
-            message, add_generation_prompt=True, tokenize=True,
-            return_dict=True, return_tensors="pt"
-        ).to(model.device, dtype=torch.bfloat16)
-        input_len = inputs["input_ids"].shape[-1]
-        with torch.inference_mode():
-            generation = model.generate(**inputs, max_new_tokens=500, do_sample=False)
-            generation = generation[0][input_len:]
+    inputs = processor.apply_chat_template(
+        messages, add_generation_prompt=True, tokenize=True,
+        return_dict=True, return_tensors="pt"
+    ).to(model.device, dtype=torch.bfloat16)
+    input_len = inputs["input_ids"].shape[-1]
+    with torch.inference_mode():
+        generation = model.generate(**inputs, max_new_tokens=500, do_sample=False)
+        generation = generation[0][input_len:]
 
-        decoded = processor.decode(generation, skip_special_tokens=True)
-        outputs.append(decoded)
+    decoded = processor.decode(generation, skip_special_tokens=True)
+    
     result_dict = {
-        'full_answer' : {
-            'prompt' : outputs[0],
-            'no_prompt' : outputs[1]
-        },
-        'succesful': {
-            'prompt' : 'no' in outputs[0].lower(),
-            'no_prompt' : 'no' in outputs[1].lower(),
-        }
+        'full_answer' : decoded,
+        'successful': 'no' in decoded[0].lower()
     }
-    count_prompt += int('no' in outputs[0].lower())
-    count_noprompt += int('no' in outputs[1].lower())
+    count += int('no' in decoded[0].lower())
     results_list.append(result_dict)
 
-print(count_noprompt, count_prompt)
+print(count)
 
 with open('results_nonmasked_garment.json', 'w') as f:
     json.dump(results_list, f, indent=2)
